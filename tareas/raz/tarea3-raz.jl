bd1a082498f0de0fa2111e2a8f5eec2e5cd5b300
mrg <miguelraz@ciencias.unam.mx>
2024-10-29 21:30:55 -0600

    avances

--- tareas/raz/tarea3-raz.jl
+++ tareas/raz/tarea3-raz.jl
@@ -51,6 +51,13 @@ function newton(f, x0, N)
     end
     x_n
 end
+function newton(f, x0::Dual, N)
+    x_n = x0
+    for i in 1:N
+        x_n -= fun(f(x_n))/der(f(x_n))
+    end
+    x_n
+end
 
 f_1(x) = x^3 - 15.625
 root = newton(f_1, 2, 100) 
@@ -60,33 +67,28 @@ root = newton(f_1, 2, 100)
 
 # (c)
 f_2(x) = x^2 - 1.1
+f_2_fijo(x) = x^2 - 1.1 - x
 
-zs = dual.(rand(10_000))
+# (d)
+raiz_fijo = newton(f_2_fijo, -0.5, 10)
+p1 = f_2(raiz_fijo)
+p2 = f_2(p1)
 
-for _ in 1:50
-    zs .= f_2.(zs)
-end
-
-zs
-
-# (d) 
-# TODO: should be 0?
-@assert count(zs .≈ f_2.(f_2.(zs))) == 0 "Deberian ser 0???"
+f_2_p2(x) = f_2(f_2(x)) - x
+raiz_p2_fijo = newton(f_2_p2, -1., 10)
+p22 = f_2(raiz_p2_fijo)
 
 # (e)
-f_3(x) = x^2 - 1
-rs = dual.(rand(10_000))
-
-for _ in 1:50
-    rs .= f_2.(rs)
-end
-
-f_3.(f_3.(rs))
-
+F(x) = x^2 - 1
+F2_fijo(x) = F(F(x)) - x 
+fijo_F2 = newton(F2_fijo, -1, 10)
+fijo_F2_2 = F(fijo_F2)
+# La derivada carga con los productos de la regla de la cadena.
 
 # --- Ejercicio 3 ---
 
 # (a)
+using Plots
 Q(x, c) = x^2 - c 
 cs = collect(0:1/4096:2)
 n = length(cs)
@@ -104,18 +106,53 @@ end
 
 plot(vcs, label = "", title = "Mapeo Q_c(x) = x^2 - c")
 
+# Re(a)
+Q(x, c) = x^2 - c 
+ex = :(Q_1(c))
+for i in 2:2^7
+    ex = :(Q_1($ex))
+    if ispow2(i)
+        op = Symbol("Q_$i")
+        @eval $op(c) = $ex
+    end
+end
+
+function Qn(c, n)
+    res = Q(0, c)
+    for i in 2:n
+        res = Q(res, c)
+    end
+    res
+end
+
+Q_1(c) = -c
+Q_2(c) = c^2 - c 
+Q_3(c) = (c^2 - c)^2 - c
+Q_4(c) = ((c^2 - c)^2 - c)^2 - c
+
+# Es claro que en esta definicion me he atorado
+# Algun tip?
+Q_1(-1.2)
+ Qn(-1.2, 1)
+Q_2(-1.2)
+ Qn(-1.2, 2)
+Q_3(-1.2)
+ Qn(-1.2, 3)
+Q_4(-1.2)
+ Qn(-1.2, 4)
+
+#=
+c1   = newton(Q_1    , 0  , 10) 
+c2   = newton(Q_2  , -1, 10) 
+c4   = newton(Q_4  , -1.4, 10) 
+c8   = newton(Q_8  , c4 , 10) 
+c16  = newton(Q_16 , c8, 10) 
+c32  = newton(Q_32 , c16, 10) 
+c64  = newton(Q_64 , c32, 10) 
+c128 = newton(Q_128, c64, 10) 
+
+cs = der.([c1, c2, c4, c8, c16, c32, c64, c128])
+fns = [der((cs[i]-cs[i+1])/(cs[i+1] - cs[i+2])) for i in 1:6]
+=#
+
 # (b)
-v1 = cs[vcs[:, 1] .== vcs[:, 2]]
-v2 = cs[vcs[:, 1] .== vcs[:, 4]]
-v3 = cs[vcs[:, 1] .== vcs[:, 8]]
-v4 = cs[vcs[:, 1] .== vcs[:, 16]]
-v5 = cs[vcs[:, 1] .== vcs[:, 32]]
-v6 = cs[vcs[:, 1] .== vcs[:, 64]]
-v7 = cs[vcs[:, 1] .== vcs[:, 128]]
-qcs = [vcs[:, 1] vcs[:, 2] vcs[:, 4] vcs[:, 8] vcs[:, 16] vcs[:, 32] vcs[:, 64] vcs[:, 128]]
-
-sorted_v1 = sort(v1)
-resv1 = - sorted_v1[1]/sorted_v1[2]
-
-sorted_v2 = sort(v2)
-resv2 = - sorted_v2[1]/sorted_v2[2]
